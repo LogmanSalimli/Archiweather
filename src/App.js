cf0175b7f8cee823f69f97ea204dff3140e19b07
Logman Salimli <archiver842@gmail.com>
2022-09-17 23:05:42 +0400

    all in one place - App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react"
+import { useSelector } from "react-redux";
 import './App.css';
+import Weather from './components/Weather';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
+  const { isDark } = useSelector(state => state.theme);
   return (
-    <div className="App">
-      <h1>Archiweather</h1>
+    <div id='app' className={isDark ? 'dark' : ''}>
+      <Header />
+      <Weather />
+      <Footer />
     </div>
   );
 }
